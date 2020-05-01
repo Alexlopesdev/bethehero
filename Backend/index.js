890d0c52eb3a4234f27309a9953c43a1320918e5
Alex Lopes de Souza <alexlopesdev@outlook.com>
2020-05-01 18:07:56 -0300

    Não esta aprecendo o Users no navegador

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 
 const app = express();
 
-app.get('/users', (request, response) => {
+app.post('/users', (request, response) => {
     return response.json ({
         evento: 'Semana Omnistack',
         aluno: 'Alex Lopes'
