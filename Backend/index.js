ae848c8e7891191e994850838484c7a3bb4c71b5
Alex Lopes de Souza <alexlopesdev@outlook.com>
2020-05-01 20:36:14 -0300

    Pensei que o problema fosse no codigo nv

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,13 @@ const express = require ('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.json({
+app.get('/users', (request, response) => {
+    return response.json({
+    
     evento: 'Semana Omnistack',
     aluno: 'Alex Lopes'
-}));
+    });
+});
 
 app.listen(3333);
 
