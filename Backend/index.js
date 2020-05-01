ea8a9dd275a6d1f82645e18267db5c9ca556486b
Alex Lopes de Souza <alexlopesdev@outlook.com>
2020-05-01 18:48:49 -0300

    não estou conseguindo resover a porcaria da rota

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,10 @@ const express = require ('express');
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    return response.json ({
-        evento: 'Semana Omnistack',
-        aluno: 'Alex Lopes'
-    }) ;
-});
+app.get('/', (request, response) => response.json({
+    evento: 'Semana Omnistack',
+    aluno: 'Alex Lopes'
+}));
 
 app.listen(3333);
+
