e55677a8de959f12b1d713a8ee4377d0ede4e9a7
Alex Lopes de Souza <alexlopesdev@outlook.com>
2020-05-02 10:34:13 -0300

    ok

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,19 @@
 const express = require ('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (request, response) => {
+app.post('/users', (request, response) => {
+    
+    const body = request.body;
+    console.log(body);
     return response.json({
     
     evento: 'Semana Omnistack',
     aluno: 'Alex Lopes'
+
+    
+
     });
 });
 
