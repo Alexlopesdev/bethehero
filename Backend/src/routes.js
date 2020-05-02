2220fbf91895ff66738bf4dbf540713bf5ef43f6
Alex Lopes de Souza <alexlopesdev@outlook.com>
2020-05-02 11:32:15 -0300

    ok

---
+++ Backend/src/routes.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+
+const routes = express.Router();
+
+routes.post('/ongs', (request, response) => {
+
+    const data = request.body;
+    console.log(data);
+    
+    return response.json()
+
+       
+    });
+
+
+
+module.exports = routes;
